3a204d52e79d86d47ca2f777c2bd7fe6625c1bab
JUST767 <geraldojustin3@gmail.com>
2026-03-10 14:30:09 +0700

    wip: implement student index page

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -6,20 +6,17 @@ class StudentController
  
     public function index()
 {
-        echo '<h1>Daftar siswa</h1>';
-        echo '<p>Menampilkan daftar siswa</p>';
+    require_once "../app/views/students/index.php";
 }
  
     public function create()
 {
-        echo '<h1>Tambah siswa</h1>';
-        echo '<p>Menampilkan form tambah siswa</p>';
+    require_once "../app/views/students/create.php";
 }
  
 public function show(string $id)
 {
-    echo 'Detail Siswa';
-    echo "<p>Menampilkan detail siswa ID: {$id}</p>";
+    require_once "../app/views/students/show.php";
 }
  
 }
