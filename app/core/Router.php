c11bfa68f6883db81c085ec228ae4001c32fe31a
JUST767 <geraldojustin3@gmail.com>
2026-04-07 14:16:37 +0700

    feat: implement student create page

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,58 +1,60 @@
-<?php
+<?php 
 namespace App\Core;
- 
-use App\Controllers\StudentController;
- 
+
+use App\Controller\StudentController;
+
 class Router
 {
-  private array $routes = [];
- 
-  public function add(string $method, string $uri, string $controller, string $function)
-  {
-      $this->routes[] = [
-         'method' => $method,
-         'uri' => $uri,
-         'controller' => $controller,
-         'function' => $function,
-      ];
-  }
- 
-  public function run ()
-{
-    $method = $_SERVER['REQUEST_METHOD'];
-    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-   
- 
-    foreach($this->routes as $route) {
-        $pattern = str_replace(
-            '{id}',
-            '([0-9]+)',
-            $route['uri'],
-        );
-        $pattern = '#^' . $pattern . '$#';
-        // /students/([0-9]+)
- 
-        if(preg_match($pattern, $uri, $matches)) {
-            array_shift($matches);
-            require_once '../app/controllers/' . $route['controller'] . '.php';
- 
-            $controllerClass = 'App\\Controllers\\' . $route['controller'];
-            $controller = new $controllerClass();
-            $function = $route['function'];
- 
-            call_user_func_array([$controller, $function],$matches);
-            // index($parameter1, $parameter2, dst);
-            // example: call_user_func_array(['StudentController', 'index'], [1,2])
-            return;
+    private array $routes = [];
+
+        public function add(string $method, string $uri, string $controller, string $function)
+        {
+            $this->routes[] = [
+                'method' => $method,
+                'uri' => $uri,
+                'controller' => $controller,
+                'function' => $function,
+            ];
         }
+
+       
+        
+    public function run()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        foreach ($this->routes as $route) {
+            $pattern = str_replace(
+                '{id}',
+                '([0-9]+)',
+                $route['uri'],
+            );
+
+            $pattern = '#^' . $pattern . '$#';
+
+            if (preg_match($pattern, $uri, $matches)) {
+                array_shift($matches);
+                require_once '../app/controllers/' . $route['controller'] . '.php';
+                $function = $route['function'];
+
+                $controllerClass = 'App\\Controller\\' . $route['controller'];
+                $controller = new $controllerClass();
+
+                call_user_func_array([$controller, $function], $matches);
+
+                return;
+            }
+        }
+
+
+
+        http_response_code(404);
+        echo "<h1>404 - Page Not Found</h1>";
     }
- 
- 
-    http_response_code(404);
-    echo '<h1>404 - Page Not Found</h1>';
- 
-    }
- 
-    }
- 
+
+
+
+}
+
 ?>
